76e13db2e155ecce0263b169862daff7ae91357a
jonathanhpak <jonathanhpak@gmail.com>
2026-03-01 21:15:33 -0800

    Add Vite app and move TodoDungeonCrawl into src

---
+++ app/src/App.jsx
@@ -0,0 +1,10 @@
+import TodoDungeonCrawl from "./ToDo-DungeonCrawl.jsx";
+
+export default function App() {
+  return (
+    <div style={{ padding: 24, color: "white", fontFamily: "system-ui" }}>
+      <h1>App is rendering ✅</h1>
+      <TodoDungeonCrawl />
+    </div>
+  );
+}
